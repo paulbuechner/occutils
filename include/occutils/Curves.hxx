38f6d78be69f02beb9f6b2819124d3b3e65bb7a0
Paul Büchner <paul-buechner@outlook.de>
2022-11-22 03:54:47 +0100

    refactor: build setup, code cleanup and more
    - introduced Google style-guidelines
    - fixed linting warnings
    - updated .gitignore

--- include/occutils/Curves.hxx
+++
@@ -1,3 +0,0 @@
-#pragma once
-// Dummy header to prevent errors if one imports Curves.hxx instead of Curve.hxx
-#include <occutils/Curve.hxx>
--- src/Pipe.cxx
+++
@@ -1,8 +0,0 @@
-#include "occutils/Pipe.hxx"
-#include <BRepOffsetAPI_MakePipe.hxx>
-
-TopoDS_Shape OCCUtils::Pipe::FromSplineAndProfile(const TopoDS_Wire& wire, const TopoDS_Shape& profile) {
-    BRepOffsetAPI_MakePipe makePipe(wire, profile);
-    makePipe.Build();
-    return makePipe.Shape();
-}
