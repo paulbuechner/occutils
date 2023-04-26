39d4f6847473a83a75685b059da653d273c45d41
Paul Büchner <paul-buechner@outlook.de>
2023-04-26 04:05:57 +0200

    chore: update project setup
    - prepare version for initial release
    - include 3rd party copyright notices in install
    - configure install

--- cmake/internal_utils.cmake
+++ cmake/internal_utils.cmake
@@ -71,3 +71,66 @@ function (target_enable_warnings target_name)
     -Wfatal-errors>
     $<$<CXX_COMPILER_ID:MSVC>:${MSVC_OPTIONS}>)
 endfunction ()
+
+########################################################################
+#
+# Configure installation
+
+# Install 3rd party copyright notices
+function (build_3rd_party_copyright)
+  set(LICENSE_3RD_PARTY_FILE ${CMAKE_CURRENT_BINARY_DIR}/LICENSE-3RD-PARTY.txt)
+  file(REMOVE ${LICENSE_3RD_PARTY_FILE}) # Delete the old file
+
+  file(GLOB SEARCH_RESULT "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/**/copyright")
+  set(COPYRIGHT_FILES ${SEARCH_RESULT} CACHE INTERNAL "copyright files")
+
+  # Handle duplicate copyright files
+  remove_duplicate_by_file_content("${COPYRIGHT_FILES}" "COPYRIGHT_FILES" "Boost Software License")
+
+  # Exclude libraries by name
+  set(EXCLUDED_LIBRARIES "vcpkg-*")
+
+  foreach (copyright_file ${COPYRIGHT_FILES})
+    string(REGEX MATCH ".*/(.*)/copyright" _ ${copyright_file})
+    if ("${CMAKE_MATCH_1}" STREQUAL "")
+      string(REGEX MATCH ".*/(.*)/LICENSE" _ ${copyright_file})
+    endif ()
+    set(LIBRARY_NAME ${CMAKE_MATCH_1})
+
+    # Check if the library name matches any of the excluded patterns
+    set(EXCLUDE_LIBRARY FALSE)
+    foreach (pattern IN ITEMS ${EXCLUDED_LIBRARIES})
+      if (LIBRARY_NAME MATCHES ${pattern})
+        set(EXCLUDE_LIBRARY TRUE)
+        break() # Exit the inner loop
+      endif ()
+    endforeach ()
+
+    if (EXCLUDE_LIBRARY)
+      continue() # Skip the current iteration
+    endif ()
+
+    file(APPEND ${LICENSE_3RD_PARTY_FILE} "-------------------------------------------------------------------------------\n")
+    file(APPEND ${LICENSE_3RD_PARTY_FILE} "${LIBRARY_NAME}\n")
+    file(APPEND ${LICENSE_3RD_PARTY_FILE} "-------------------------------------------------------------------------------\n")
+    file(READ ${copyright_file} COPYRIGHT_CONTENTS)
+    file(APPEND ${LICENSE_3RD_PARTY_FILE} "${COPYRIGHT_CONTENTS}\n")
+  endforeach ()
+endfunction ()
+
+# Remove duplicate from list by content
+function (remove_duplicate_by_file_content list list_name filter_by)
+  set(INCLUDED OFF)
+  foreach (file ${list})
+    file(STRINGS ${file} content)
+    string(FIND "${content}" ${filter_by} FOUND)
+    if (NOT ${FOUND} EQUAL -1)
+      if (INCLUDED)
+        list(REMOVE_ITEM list ${file})
+      else ()
+        set(INCLUDED ON)
+      endif ()
+    endif ()
+  endforeach ()
+  set(${list_name} ${list} PARENT_SCOPE)
+endfunction ()
