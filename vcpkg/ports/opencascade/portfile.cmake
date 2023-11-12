2e6a9962f190c6f3d22d248b80b2c54de63a3764
Paul Büchner <paul-buechner@outlook.de>
2023-11-12 23:20:24 +0100

    chore(deps): update `opencascade` to 7.7.2#2

--- vcpkg/ports/opencascade/portfile.cmake
+++ vcpkg/ports/opencascade/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO Open-Cascade-SAS/OCCT
-  REF cec1ecd0c9f3b3d2572c47035d11949e8dfa85e2 #V7_7_2
+  REF cec1ecd0c9f3b3d2572c47035d11949e8dfa85e2 #V7.7.2
   SHA512 2fe98eadd7f9b922729bf80b56f260729d1c257c41392e4be4f070667ee77e94e2b286a873430b41ea61076acf1388aee7ba8b91789aa6199db56066796bb2d3
   HEAD_REF master
   PATCHES
@@ -9,15 +9,10 @@ vcpkg_from_github(
   fix-install-prefix-path.patch
   install-include-dir.patch
   fix-depend-freetype.patch
+  fix-depend-vtk.patch
   fix-dependence.patch
   fix-find-tbb.patch
-  )
-
-# MinGW compiler does generate import libraries (.dll.a files) for DLLs on Windows.
-# Suppresses Warning: Import libraries were not present (only looking for ".lib" files)
-if (WIN32 AND VCPKG_CMAKE_SYSTEM_NAME STREQUAL "MinGW")
-  set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
-endif ()
+)
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
   set(BUILD_TYPE "Shared")
@@ -31,11 +26,10 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
                      "tbb" USE_TBB
                      "rapidjson" USE_RAPIDJSON
                      "samples" INSTALL_SAMPLES
-                     )
+                     "vtk" USE_VTK
+)
 
-# VTK option in opencascade not currently supported because only 6.1.0 is supported but vcpkg has >= 9.0
-
-# We turn off BUILD_MODULE_Draw as it requires TCL 8.6 and TK 8.6 specifically which conflicts with vcpkg only having TCL 9.0 
+# We turn off BUILD_MODULE_Draw as it requires TCL 8.6 and TK 8.6 specifically which conflicts with vcpkg only having TCL 9.0
 # And pre-built ActiveTCL binaries are behind a marketing wall :(
 # We use the Unix install layout for Windows as it matches vcpkg
 vcpkg_cmake_configure(
@@ -49,9 +43,11 @@ vcpkg_cmake_configure(
   -DBUILD_SAMPLES_QT=OFF
   -DBUILD_DOC_Overview=OFF
   -DINSTALL_TEST_CASES=OFF
-  )
+  -DBUILD_MODULE_DETools=OFF
+)
 
 vcpkg_cmake_install()
+
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/opencascade)
 
 #make occt includes relative to source_file
@@ -63,7 +59,7 @@ list(APPEND ADDITIONAL_HEADERS
      "NCollection_Haft.h"
      "OSD_PerfMeter.h"
      "Standard_values.h"
-     )
+)
 
 file(GLOB files "${CURRENT_PACKAGES_DIR}/include/opencascade/[a-zA-Z0-9_]*\.[hgl]xx")
 foreach (file_name IN LISTS files)
@@ -93,9 +89,8 @@ if (NOT VCPKG_BUILD_TYPE)
        "${CURRENT_PACKAGES_DIR}/share/opencascade/OpenCASCADEFoundationClassesTargets-debug.cmake"
        "${CURRENT_PACKAGES_DIR}/share/opencascade/OpenCASCADEModelingAlgorithmsTargets-debug.cmake"
        "${CURRENT_PACKAGES_DIR}/share/opencascade/OpenCASCADEModelingDataTargets-debug.cmake"
-       "${CURRENT_PACKAGES_DIR}/share/opencascade/OpenCASCADEDEToolsTargets-debug.cmake"
        "${CURRENT_PACKAGES_DIR}/share/opencascade/OpenCASCADEVisualizationTargets-debug.cmake"
-       )
+  )
 
   foreach (TARGET_FILE IN LISTS TARGET_FILES)
     file(READ "${TARGET_FILE}" filedata)
@@ -115,14 +110,16 @@ if (VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
 
   # the bin directory ends up with bat files that are noise, let's clean that up
   file(GLOB BATS "${CURRENT_PACKAGES_DIR}/bin/*.bat")
-  if (BATS)
-    file(REMOVE_RECURSE ${BATS})
-  endif ()
-
-  vcpkg_copy_tools(TOOL_NAMES ExpToCasExe AUTO_CLEAN)
+  file(REMOVE_RECURSE ${BATS})
 else ()
   # remove scripts in bin dir
   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif ()
+
+if (INSTALL_SAMPLES)
+  foreach (dir "Tutorial" "FuncDemo" "IESample" "OCCTOverview")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/opencascade/samples/qt/${dir}/env.sh" "${CURRENT_PACKAGES_DIR}/bin/env.sh" "<not/existing>")
+  endforeach ()
 endif ()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/OCCT_LGPL_EXCEPTION.txt")
