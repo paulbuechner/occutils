b8ab31413430b158e0474d2084631b233d2a605b
Paul Büchner <paul-buechner@outlook.de>
2024-04-25 20:37:03 +0200

    fix(shape_components): add missing header include

--- include/occutils/occutils-shape-components.h
+++ include/occutils/occutils-shape-components.h
@@ -12,6 +12,7 @@
 #include <TopoDS_Edge.hxx>
 #include <TopoDS_Face.hxx>
 #include <TopoDS_Shape.hxx>
+#include <TopoDS_Shell.hxx>
 #include <TopoDS_Solid.hxx>
 #include <TopoDS_Vertex.hxx>
 #include <TopoDS_Wire.hxx>
--- src/occutils-shape-components.cc
+++ src/occutils-shape-components.cc
@@ -11,6 +11,7 @@
 #include <TopoDS_Edge.hxx>
 #include <TopoDS_Face.hxx>
 #include <TopoDS_Shape.hxx>
+#include <TopoDS_Shell.hxx>
 #include <TopoDS_Solid.hxx>
 #include <TopoDS_Vertex.hxx>
 #include <TopoDS_Wire.hxx>
